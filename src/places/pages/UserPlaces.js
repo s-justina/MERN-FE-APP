25693950f30cf1f5c3e23c472572d72e5992d388
Justyna <jus.sobcz@gmail.com>
2024-02-09 14:48:34 +0100

    feat: connect the user places to the BE

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,36 +1,38 @@
+import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {PlaceList} from "../components/PlaceList";
-
-const DUMMY_PLACES = [
-    {
-        id: "p1",
-        title: "Empire State Building",
-        description: "One of the most famous sky scrapers in the world!",
-        imageUrl: "https://travel.usnews.com/dims4/USNEWS/4aee1bb/2147483647/resize/976x652%5E%3E/crop/976x652/quality/85/?url=https%3A%2F%2Ftravel.usnews.com%2Fimages%2FEmpire_State_Building_Getty_Zsolt_Hlinka.jpg",
-        address: "20 W 34th St, New York, NY 10001",
-        location: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        },
-        creator: "u1"
-    },
-    {
-        id: "p2",
-        title: "Empire State Building",
-        description: "One of the most famous sky scrapers in the world!",
-        imageUrl: "https://travel.usnews.com/dims4/USNEWS/4aee1bb/2147483647/resize/976x652%5E%3E/crop/976x652/quality/85/?url=https%3A%2F%2Ftravel.usnews.com%2Fimages%2FEmpire_State_Building_Getty_Zsolt_Hlinka.jpg",
-        address: "20 W 34th St, New York, NY 10001",
-        location: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        },
-        creator: "u2"
-    }
-]
+import {useHttpClient} from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 export const UserPlaces = () => {
     const userId = useParams().userId;
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+    const [loadedPlaces, setLoadedPlaces] = useState();
+    const {isLoading, error, sendRequest, clearError} = useHttpClient();
 
-    return <PlaceList items={loadedPlaces}/>
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            try {
+                const responseData = await sendRequest(`${process.env.REACT_APP_API_BASE_URL}/places/user/${userId}`);
+
+                setLoadedPlaces(responseData.places);
+            } catch (err) {
+            }
+        }
+
+        fetchPlaces();
+
+    }, [sendRequest, userId])
+
+    return (
+        <>
+            <ErrorModal error={error} onClear={clearError}/>
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner/>
+                </div>
+            )}
+            {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces}/>}
+        </>
+    )
 }
