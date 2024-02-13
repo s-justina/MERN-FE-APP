98392825c738b6c73940e3567bd62a26cdade422
Justyna <jus.sobcz@gmail.com>
2024-02-13 20:32:56 +0100

    feat: add new secret variable

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -62,7 +62,7 @@ export const PlaceItem = props => {
                 <Card className="place-item__content">
                     {isLoading && <LoadingSpinner asOverlay/>}
                     <div className="place-item__image">
-                        <img src={`http://localhost:5000/${props.image}`} alt={props.title}/>
+                        <img src={`${process.env.REACT_APP_ASSET_URL}/${props.image}`} alt={props.title}/>
                     </div>
                     <div className="place-item__info">
                         <h2>{props.title}</h2>
